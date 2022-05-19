2df786241de5031667c0112a2ca55aef3f079d0e
Kim Jørgensen <kj@daniasoftware.com>
2022-05-19 15:50:10 +0200

    Added backend

--- src/ApiClients/Files.js
+++ src/ApiClients/Files.js
@@ -1,3 +1,10 @@
 export function GetFiles() {
-  return fetch(`https://${process.env.REACT_APP_ENDPOINT}/Files`).then((x) => x.json());
+	return fetch(`https://${process.env.REACT_APP_ENDPOINT}/Files`).then((x) => x.json());
+}
+
+export function GetLink(filename) {
+	return fetch(`https://${process.env.REACT_APP_ENDPOINT}/Files/GetEditLink/` + filename).then((x) => x.json());
+}
+export function Cancel(id) {
+	return fetch(`https://${process.env.REACT_APP_ENDPOINT}/Files/Cancel/` + id);
 }
--- src/Components/UserInformation.js
+++ src/Components/UserInformation.js
@@ -1,59 +1,59 @@
 import { UserIcon } from "@heroicons/react/outline";
 export default function UserInformation() {
-  return (
-    <div>
-      <h1 className="text-xl font-semibold text-gray-900 mt-5 ml-8">
-        <div className="flex flex-row">
-          <UserIcon className="h-5 w-5 mt-1"></UserIcon>
-          <div className="-pt-2">User information</div>
-        </div>
-      </h1>
-      <div className="w-2/5 border border-indigo-100 rounded-md ml-8 mt-3 shadow ">
-        <div className="flex flex-row m-5">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Given name
-            </label>
-            <div className="mt-1">
-              <input type="text" name="givenname" id="givenname" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-            </div>
-          </div>
-          <div className="ml-5 grow">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Sure name
-            </label>
-            <div className="mt-1">
-              <input type="text" name="surename" id="surename" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-row m-5">
-          <div className="grow ">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Address
-            </label>
-            <input type="text" name="surename" id="surename" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-          </div>
-        </div>
-        <div className="flex flex-row m-5">
-          <div>
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              Zip code
-            </label>
-            <div className="mt-1">
-              <input type="text" name="givenname" id="givenname" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-            </div>
-          </div>
-          <div className="ml-5 grow">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-              City
-            </label>
-            <div className="mt-1 ">
-              <input type="text" name="surename" id="surename" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div>
+			<h1 className="text-xl font-semibold text-gray-900 mt-5 ml-8">
+				<div className="flex flex-row">
+					<UserIcon className="h-5 w-5 mt-1"></UserIcon>
+					<div className="-pt-2">User information</div>
+				</div>
+			</h1>
+			<div className="w-2/5 border border-indigo-100 rounded-md ml-8 mt-3 shadow ">
+				<div className="flex flex-row m-5">
+					<div>
+						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+							Given name
+						</label>
+						<div className="mt-1">
+							<input type="text" name="givenname" id="givenname" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+						</div>
+					</div>
+					<div className="ml-5 grow">
+						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+							Surname
+						</label>
+						<div className="mt-1">
+							<input type="text" name="surname" id="surenamlse" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+						</div>
+					</div>
+				</div>
+				<div className="flex flex-row m-5">
+					<div className="grow ">
+						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+							Address
+						</label>
+						<input type="text" name="surename" id="surename" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+					</div>
+				</div>
+				<div className="flex flex-row m-5">
+					<div>
+						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+							Zip code
+						</label>
+						<div className="mt-1">
+							<input type="text" name="givenname" id="givenname" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+						</div>
+					</div>
+					<div className="ml-5 grow">
+						<label htmlFor="email" className="block text-sm font-medium text-gray-700">
+							City
+						</label>
+						<div className="mt-1 ">
+							<input type="text" name="surename" id="surename" className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
